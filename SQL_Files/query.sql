2177e17de133b1605c2fd541c0bf2f7005456509
george-simon <george-simon@users.noreply.github.com>
2020-09-18 10:11:23 -0700

    sql schema folder created

---
+++ SQL_Files/query.sql
@@ -0,0 +1,8 @@
+
+-- Testing that uploaded data from 2019_US_County_Health_Rankings.csv works
+-- NOTE: Need to wrap table 2019_US_County_Health_Rankings in "" because it starts with a digit
+SELECT *
+FROM "2019_US_County_Health_Rankings"
+LIMIT 5;
+
+
